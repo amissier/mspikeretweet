6d8818d9709bd5bd85f3cedf36ad3bb2186972c8
Prab <prab.missier@gmail.com>
2018-02-23 19:38:31 +1100

    updated

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-/* All keys are hidden in 'Config variables within Heroku, we wouldn't want your secret keys to be on public display */
-module.exports = {
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token_key: process.env.access_token_key,
-  access_token_secret: process.env.access_token_secret
+{
+  consumer_key: "EeoJusVzPqAWMriZ1eADrYb39",
+  consumer_secret: "izauqcA2zYHbIci1YofPoTlXM9oVOGaHM7tJSowcGgLLuc4uQ8",
+  access_token_key: "57567113-OZVNjGKQW91SV8YxHCxLT1Fh70e6QKbWyD3tnAkkz",
+  access_token_secret: "VhjJBWXToj7vYTxUoyFetWInUR4T6FQMY3osdkbC7aw9d"
 }
